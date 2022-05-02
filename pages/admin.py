11eacfae40757800d700b1885031495389ba2fd8
javier ferrales <ferralespereira@gmail.com>
2022-05-02 18:49:34 -0400

    setting admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+admin.site.register(Page)
+
+# Settings of Panel
+title    = "Gestion Panel"
+subtitle = "BlogDjango"
+
+admin.site.site_header = title
+admin.site.site_title  = title
+admin.site.index_title = subtitle
