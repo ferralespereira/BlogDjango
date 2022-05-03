7fd629906f05497f644a7868abea831fee1d3dec
javier ferrales <ferralespereira@gmail.com>
2022-05-03 16:43:33 -0400

    details

--- pages/models.py
+++ pages/models.py
@@ -9,9 +9,9 @@ class Page(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name="Updated at")
 
+    def __str__(self):
+        return self.title
+
 class Meta:
     verbose_name        = "Page"
     verbose_name_plural = "Pages"
-
-def __str__(self):
-    return self.title
