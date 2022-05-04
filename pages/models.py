f927c8c5409101f1fa0a8cb84b8da56f220a9835
javier ferrales <ferralespereira@gmail.com>
2022-05-04 18:23:06 -0400

    creating RichTextField

--- pages/models.py
+++ pages/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Page(models.Model):
     title      = models.CharField(max_length=50, verbose_name="Title")  
-    content    = models.TextField(verbose_name="Content")
+    content    = RichTextField(verbose_name="Content")
     slug       = models.CharField(unique=True, max_length=150, verbose_name="URL amigable")
     visible    = models.BooleanField(verbose_name="Visible?")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name="Updated at")
 
-    def __str__(self):
-        return self.title
-
     class Meta:
         verbose_name        = "Page"
         verbose_name_plural = "Pages"
+
+    def __str__(self):
+        return self.title
