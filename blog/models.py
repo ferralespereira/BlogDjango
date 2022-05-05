90cc8310e933aafaec4303977031e5a721bcbe2b
javier ferrales <ferralespereira@gmail.com>
2022-05-05 18:34:01 -0400

    creating Category Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="name")
+    description = models.CharField(max_length=255, verbose_name="description")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="created_at")
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categorys"
+
+    
+    def __str__(sefl):
+        return sefl.name
