25d921c9953f6cebad2be00f9d435adc90e9b83a
javier ferrales <ferralespereira@gmail.com>
2022-05-05 18:15:36 -0400

    creating order field into db pages table

--- pages/models.py
+++ pages/models.py
@@ -6,6 +6,7 @@ class Page(models.Model):
     title      = models.CharField(max_length=50, verbose_name="Title")  
     content    = RichTextField(verbose_name="Content")
     slug       = models.CharField(unique=True, max_length=150, verbose_name="URL amigable")
+    order      = models.IntegerField(default=0, verbose_name="order")
     visible    = models.BooleanField(verbose_name="Visible?")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name="Updated at")
@@ -15,4 +16,4 @@ class Page(models.Model):
         verbose_name_plural = "Pages"
 
     def __str__(self):
-        return self.title
+        return "title: "+self.title+", order: "+str(self.order)
