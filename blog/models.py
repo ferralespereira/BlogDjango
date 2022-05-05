8454adcecf35981753fc35600eda42bc73f774d4
javier ferrales <ferralespereira@gmail.com>
2022-05-05 19:09:43 -0400

    creating tables from Blog App

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -13,3 +15,22 @@ class Category(models.Model):
     
     def __str__(sefl):
         return sefl.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=150, verbose_name="title")
+    content = RichTextField(verbose_name="content")
+    image = models.ImageField(default='null', verbose_name="image")
+    public = models.BooleanField(verbose_name="Public?")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="created_at")
+    user = models.ForeignKey(User, verbose_name="user", on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, verbose_name="categories")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="updated_at")
+
+    class Meta:
+        verbose_name = "article"
+        verbose_name_plural = "articles"
+
+    
+    def __str__(sefl):
+        return sefl.title
