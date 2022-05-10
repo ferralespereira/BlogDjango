6712b9cc0d7a9980a5c8b5fe2c563a34599323ac
javier ferrales <ferralespereira@gmail.com>
2022-05-10 17:02:38 -0400

    making subqueries

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,8 +1,16 @@
-from blog.models import Category
+from blog.models import Category, Article
 
 def get_categories(request):
-    categories = Category.objects.values_list('id', 'name')
+
+    # subquery
+
+    # categories that have been used by Artilces registers
+    categories_in_use = Article.objects.filter(public=True).values_list('categories', flat=True)
+    
+    # id and name of each of these categories that have been in use
+    categories = Category.objects.filter(id__in=categories_in_use).values_list('id', 'name')
 
     return {
-        'categories': categories
+        'categories': categories,
+        'categories_in_use': categories_in_use
     }
