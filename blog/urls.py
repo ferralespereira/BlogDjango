ee2af1ab26eba06464a125f14db51306b9e6451d
javier ferrales <ferralespereira@gmail.com>
2022-05-11 18:03:04 -0400

    showing article

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('articles/', views.list, name='articles'),
+    path('article/<int:article_id>', views.article, name='article'),
     path('category/<int:category_id>', views.category, name='category'),
 ]
