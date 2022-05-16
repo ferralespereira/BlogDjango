0500e4560847d7aab65b12c2f8b7a2486f2d7617
javier ferrales <ferralespereira@gmail.com>
2022-05-16 18:21:14 -0400

    creating regiester view

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,3 +10,9 @@ def about(request):
     return render(request, 'mainapp/about.html', {
         'title': 'About'
     })
+
+def register_page(request):
+    
+    return render(request, 'users/register.html', {
+        'title': 'Register'
+    })
